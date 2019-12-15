5c19d4fb4f0e1438e522a95b8753fea266227c19
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-12-15 12:50:13 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:clipboard_manager/clipboard_manager.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
+    MaterialApp(home: MyApp()),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Builder(
+        builder: (cntx) => SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -18,13 +34,138 @@ void main() {
                 style: TextStyle(
                     fontSize: 40.0,
                     color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.w200,
                     fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontFamily: 'Sans',
+                    fontSize: 18.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  launch("tel:919023481563");
+                },
+                onLongPress: () {
+                  ClipboardManager.copyToClipBoard(
+                          "Piyush Garg : +919023481563")
+                      .then((result) {
+                    final snackBar = SnackBar(
+                      content: Text('Copied to Clipboard'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {},
+                      ),
+                    );
+                    Scaffold.of(cntx).showSnackBar(snackBar);
+                  });
+                },
+                child: Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 9023481563',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Sans',
+                          fontSize: 20.0),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  launch('mailto:gargpiyush195@gmail.com');
+                },
+                onLongPress: () {
+                  ClipboardManager.copyToClipBoard("gargpiyush195@gmail.com")
+                      .then((result) {
+                    final snackBar = SnackBar(
+                      content: Text('Copied to Clipboard'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {},
+                      ),
+                    );
+                    Scaffold.of(cntx).showSnackBar(snackBar);
+                  });
+                },
+                child: Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'gargpiyush195@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Sans',
+                          fontSize: 18.0),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  launch('https://github.com/piyushgarg195');
+                },
+                onLongPress: () {
+                  ClipboardManager.copyToClipBoard(
+                          "Piyush Garg: https://github.com/piyushgarg195")
+                      .then((result) {
+                    final snackBar = SnackBar(
+                      content: Text('Copied to Clipboard'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {},
+                      ),
+                    );
+                    Scaffold.of(cntx).showSnackBar(snackBar);
+                  });
+                },
+                child: Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.code,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'GitHub',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Sans',
+                          fontSize: 20.0),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
