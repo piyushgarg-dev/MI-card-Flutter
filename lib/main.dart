5e0c09d501c1a205e5faa5877189780d24f5e719
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-12-15 11:52:26 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.teal,
+                backgroundImage: AssetImage('images/myself.jpeg'),
+              ),
+              Text(
+                'Piyush Garg',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
